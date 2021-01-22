182f0c6bc22a884872fdbd844639a4226a0be2bf
deren2525 <deren2525@gmail.com>
2021-01-23 02:23:51 +0900

    update: 2.1.0

--- js/newTabNote.js
+++ js/newTabNote.js
@@ -1,8 +1,9 @@
 const initText =
-  "## Welcome to NewTabNote!!\n\nThank you for downloading NewTabNote! 🎉\nNewTabNote a new tab extension for notes 📖\nLet's edit by pressing the pencil mark in the upper left!\n\n## Example\n\n### Emphasis\n*This text will be italic.*\n**This text will be bold.**\n*You **can** combine them.*\n\n### Lists\n\n####  Unordered\n* Item 1\n* Item 2\n  * Item 2a\n  * Item 2b\n\n#### Ordered\n1. Item 1\n2. Item 2\n3. Item 3\n\n### Link\n[Chrome Web Store](https://chrome.google.com/webstore/category/extensions?hl=en&)\n\n### Task Lists\n- [ ] Task 1\n- [X] Task 2\n\n### Blockquote\n> **note**\n> element is used to indicate the quotation of a large section of text from another source.\n\n### Code\n```\nif (isAwesome) {\n  return true\n}\n```\n";
+  "# Welcome to NewTabNote!!\n\nThank you for downloading NewTabNote! 🎉\nNewTabNote a new tab extension for notes 📖\nLet's edit by pressing the pencil mark in the upper left!\n\n## Example\n\n### ■ Emphasis\n*This text will be italic.*\n**This text will be bold.**\n*You **can** combine them.*\n\n### ■ Lists\n\n####  Unordered\n* Item 1\n* Item 2\n  * Item 2a\n  * Item 2b\n\n#### Ordered\n1. Item 1\n2. Item 2\n3. Item 3\n\n### ■ Link\n[Chrome Web Store](https://chrome.google.com/webstore/category/extensions?hl=en&)\n\n### ■ Task Lists\n- [ ] Task 1\n- [X] Task 2\n\n### ■ Blockquote\n> **note**\n> element is used to indicate the quotation of a large section of text from another source.\n\n### ■ Code\n```\nif (isAwesome) {\n  return true\n}\n```\n";
 
 // Input
 const editInput = document.getElementById("edit-input");
+const editInputLineHeight = parseInt(editInput.style.lineHeight);
 // Window
 const editWindow = document.getElementById("edit-window");
 const previewWindow = document.getElementById("preview-window");
@@ -14,36 +15,47 @@ const modePreviewButton = document.getElementById("mode-preview");
 
 window.onload = () => {
   const height = window.innerHeight - 35;
-  editWindow.style.height = height + "px";
-  previewWindow.style.height = height + "px";
+  editInput.style.height = height + "px";
+  previewWindow.style.height = height + 35 + "px";
 
   if (localStorage.getItem("new_tab_note")) {
-    editInput.innerText = localStorage.getItem("new_tab_note");
+    // editInput.innerText = localStorage.getItem("new_tab_note");
+    editInput.value = localStorage.getItem("new_tab_note").replace(/\s+$/, '');
     previewWindow.innerHTML = marked(localStorage.getItem("new_tab_note"));
+
+    console.log(editInput.value)
   } else {
     localStorage.setItem("new_tab_note_init", initText);
-    editInput.innerText = localStorage.getItem("new_tab_note_init");
+    editInput.value = localStorage.getItem("new_tab_note_init");
     previewWindow.innerHTML = marked(localStorage.getItem("new_tab_note_init"));
   }
 };
 
 editInput.addEventListener("input", () => {
-  // Save to local storage
-  localStorage.setItem("new_tab_note", editInput.innerText);
+  saveToLocalStorage(editInput.value);
 
-  // Convert to markdown
-  convertedText = marked(editInput.innerText);
-
-  // Convert [] [x] to checkbox
-  previewWindow.innerHTML = convertedText.replace(
-    /\[x\]/g,
-    '<input type="checkbox" checked="checked">'
-  );
-  previewWindow.innerHTML = convertedText.replace(
-    /\[ \]/g,
-    '<input type="checkbox">'
-  );
+  // 改行でテキストエリア の高さ自動調整
+  const lines = (editInput.value + '\n').match(/\n/g).length;
+  editInput.style.height = lines * editInputLineHeight
 });
+
+editInput.addEventListener("keydown", (e) => {
+  if (!e) return
+  console.log(this)
+  if (e.key === "Tab") {
+    e.preventDefault();
+
+    // 現在のカーソルの位置
+    const cursorPosition = editInput.selectionStart;
+    // カーソルの左右の文字列を取得
+    const cursorLeft = editInput.value.substr(0, cursorPosition);
+    const cursorRight = editInput.value.substr(cursorPosition, editInput.value.length);
+    editInput.value = cursorLeft + "　" + cursorRight;
+    // カーソルの位置を入力したタブの後ろにする
+    editInput.selectionEnd = cursorPosition + 1;
+    saveToLocalStorage(editInput.value);
+  }
+})
 
 // Mode button click
 modeEditButton.addEventListener("click", (e) => {
@@ -72,13 +84,29 @@ function modeButtonStatus(e) {
   });
 }
 
+function saveToLocalStorage(value) {
+  // Save to local storage
+  localStorage.setItem("new_tab_note", value);
+  // Convert to markdown
+  convertedText = marked(value);
+  // Convert [] [x] to checkbox
+  previewWindow.innerHTML = convertedText.replace(
+    /\[x\]/g,
+    '<input type="checkbox" checked="checked">'
+  );
+  previewWindow.innerHTML = convertedText.replace(
+    /\[ \]/g,
+    '<input type="checkbox">'
+  );
+}
+
 // Window risize
 window.addEventListener(
   "resize",
   function () {
     const height = window.innerHeight - 35;
-    editWindow.style.height = height + "px";
-    previewWindow.style.height = height + "px";
+    editInput.style.height = height + "px";
+    previewWindow.style.height = height + 35 + "px";
   },
   false
 );
